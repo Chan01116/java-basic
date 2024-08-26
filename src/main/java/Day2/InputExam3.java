8d39724fb0b684c1eac88949e41034592e7de939
chan0116 <ktiun9630>
2024-08-26 17:30:51 +0900

    aug 26th

--- src/main/java/Day2/InputExam3.java
+++ src/main/java/Day2/InputExam3.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class InputExam3 {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("이름을 입력해 주세요 : ");
+//        String str = sc.nextLine();
+//        System.out.print("나이를 입력해 주세요 : ");
+//        String str2 = sc.nextLine();
+//        System.out.println("안녕하세요 " + str2 +"세 " + str + "입니다.");
         Scanner sc = new Scanner(System.in);
-        System.out.print("이름을 입력해 주세요 : ");
-        String str = sc.nextLine();
-        System.out.print("나이를 입력해 주세요 : ");
-        String str2 = sc.nextLine();
-        System.out.println("안녕하세요 " + str2 +"세 " + str + "입니다.");
+        System.out.print("원하는 단을 입력해주세요.");
+        int num = sc.nextInt();
+        if(num == 2){ for(int i = 2; i<=9; i++);{
+            System.out.println(i + "X" + );
+        }
+
+        }
     }
+
 }
